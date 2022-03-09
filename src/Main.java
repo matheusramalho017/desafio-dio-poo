399ab2677d30cc38f473d189b695623bcd5c4954
matheusramalho017 <66134948+matheusramalho017@users.noreply.github.com>
2022-03-09 10:26:10 -0300

    terceiro commit

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,30 @@
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
 
         Curso curso1 = new Curso();
+        curso1.setTitulo("curso de Java");
+        curso1.setDescricao("Curso feito na DIO");
+        curso1.setCargaHoraria(10);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("curso de JS");
+        curso2.setDescricao("Curso feito na DIO");
+        curso2.setCargaHoraria(15);
+
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("Mentoria de Java");
+        mentoria1.setDescricao("Ensino de Java");
+        mentoria1.setData(LocalDate.now());
 
         System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria1);
+
 
     }
 }
